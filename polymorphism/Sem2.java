9b3a052eec82d4b05bf3be9b544347c289737d47
Programmer-RD-AI <go2ranuga@gmail.com>
2022-06-26 17:09:45 +0530

    Create Sem2.java
    Co-Authored-By: Ranuga <79456372+Programmer-RD-AI@users.noreply.github.com>

---
+++ polymorphism/Sem2.java
@@ -0,0 +1,18 @@
+class Employee {
+    public void calculate(int num1, int num2) {
+        System.out.println("Total of two numbers" + (num1 + num2));
+    }
+
+    public void calculate(int num1, int num2, int num3) {
+        System.out.println("TOT" + (num1 + num2 + num3));
+    }
+}
+
+
+class Sem2 {
+    public static void main(String[] args) {
+        Employee e1 = new Employee();
+        e1.calculate(1, 2);
+        e1.calculate(1, 2, 3);
+    }
+}

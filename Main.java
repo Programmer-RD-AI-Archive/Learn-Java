1c9221b5a31abfb0e2bc6fdfcc122fa37662347b
Ranuga <79456372+Programmer-RD-AI@users.noreply.github.com>
2024-08-20 06:06:22 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,12 +1,5 @@
-import javax.swing.JOptionPane;
-
-public class Main {
-    public static void main(String[] args) {
-        String name = JOptionPane.showInputDialog("Enter your name: ");
-        JOptionPane.showMessageDialog(null, "Hello " + name);
-        // System.out.println("Ur name is: "+ name);
-        int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age :"));
-        JOptionPane.showMessageDialog(null, "You are " + age + " years old");
-        double height = Double.parseDouble(JOptionPane.showInputDialog("Entre your height"))
+public class Main{
+    public static void main(String[] args){
+        return;
     }
 }
